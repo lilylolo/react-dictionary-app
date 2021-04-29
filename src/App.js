866a166433b663996e63c53fa901624586cc674d
lilylolo <78271372+lilylolo@users.noreply.github.com>
2021-04-29 16:15:23 +0100

    Rendered word meanings

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import Dictionary from './Dictionary';
 
+
 function App() {
   return (
-    <div className="App">
+    <div className="App-wrapper">
       <header className="App-header">
        Dictionary
       </header>

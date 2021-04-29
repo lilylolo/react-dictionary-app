f274ddc59e4005fb2f4bb8069f72f8914b653788
lilylolo <78271372+lilylolo@users.noreply.github.com>
2021-04-29 16:24:23 +0100

    Footer added

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Dictionary from './Dictionary';
 
 function App() {
   return (
+    <div className="Container">
     <div className="App-wrapper">
       <header className="App-header">
        Dictionary
       </header>
       <Dictionary />
-
+    </div>
+    <footer> <a href="https://github.com/lilylolo/react-dictionary-app" rel="noreferrer" target="_blank">Open-source</a> code by <a href="https://www.linkedin.com/in/lily-lo-9068aa12a" rel="noreferrer" target="_blank">Lily Lo</a></footer>
     </div>
   );
 }

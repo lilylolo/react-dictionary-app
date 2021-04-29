c27f159f7eb7888edbc4bf459ac42441577b7465
lilylolo <78271372+lilylolo@users.noreply.github.com>
2021-04-29 16:59:51 +0100

    Implemented phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Phonetic(props) {
+    return (
+        <div className="Phonetic">
+            {props.phonetic.text} <a href={props.phonetic.audio} target="_blank">Listen</a>
+        </div>
+    )
+
+}
